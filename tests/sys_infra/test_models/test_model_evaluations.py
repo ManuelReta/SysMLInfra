6d7af4b4e3c4d304d90f8a36ee1b2946c1940026
Sindre Kaald <sindre.kaald@dnv.com>
2026-06-25 15:02:44 +0200

    Mark with kernel

--- tests/sys_infra/test_models/test_model_evaluations.py
+++ tests/sys_infra/test_models/test_model_evaluations.py
@@ -1,19 +1,23 @@
 from sys_infra.environment import INTEGRATION_EXAMPLES_DIR
 from sys_infra.verify import Pipeline
+import pytest
 
 
+@pytest.mark.sysmlkernel
 def test_layered_pump() -> None:
     p = Pipeline(INTEGRATION_EXAMPLES_DIR / "layered_simple_pump")
     results = p(publish=False)
     assert results
 
 
+@pytest.mark.sysmlkernel
 def test_rollup() -> None:
     p = Pipeline(INTEGRATION_EXAMPLES_DIR / "rollup")
     results = p(publish=False)
     assert results
 
 
+@pytest.mark.sysmlkernel
 def test_simple_pump() -> None:
     p = Pipeline(INTEGRATION_EXAMPLES_DIR / "simple_pump")
     results = p(publish=False)
